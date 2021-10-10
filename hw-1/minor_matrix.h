a9bed4f512f94e02cf2c63ae0225b232a074aa82
shevvvo <92267313+shevvvo@users.noreply.github.com>
2021-10-10 20:18:30 +0300

    hw-1

---
+++ hw-1/minor_matrix.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void free_memory(int** matrix, size_t rows);
+int** finding_minor(int** matrix, size_t rows, size_t cols, size_t index_i, size_t index_j);
+int** init_memory(size_t rows, size_t cols);
