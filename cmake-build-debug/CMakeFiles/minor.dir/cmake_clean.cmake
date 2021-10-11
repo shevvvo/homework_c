1a3bc9eabff3244a33a45b0136eb6f67d591657e
shevvvo <vasily.shevchenko2222222@gmail.com>
2021-10-11 23:25:42 +0300

    changed x2

---
+++ cmake-build-debug/CMakeFiles/minor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/minor.dir/project/minor_matrix.c.o"
+  "libminor.a"
+  "libminor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/minor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/runTests.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runTests.dir/tests/minor_matrix_test.cpp.o"
+  "runTests"
+  "runTests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
