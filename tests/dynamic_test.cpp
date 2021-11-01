38115f8a7d63a54c55d4c01a73ba6bd5a4b5208e
shevvvo <vasily.shevchenko2222222@gmail.com>
2021-11-01 21:35:00 +0300

    x

--- tests/dynamic_test.cpp
+++ tests/dynamic_test.cpp
@@ -4,31 +4,31 @@ extern "C" {
 #include "functions.h"
 }
 
-TEST(STATIS_TEST, wrong_filename) {
+TEST(DYNAMIC_TEST, wrong_filename) {
     EXPECT_EQ(start_work("error"), FILE_ERROR);
 }
 
-TEST(STATIC_TEST, zero_size) {
+TEST(DYNAMIC_TEST, zero_size) {
     EXPECT_EQ(start_work("../tests/3.txt"), FILE_ERROR);
 }
 
-TEST(STATIC_TEST, test1) {
+TEST(DYNAMIC_TEST, test1) {
     EXPECT_EQ(start_work("../tests/2.txt"), 1);
 }
 
-TEST(STATIC_TEST, test2) {
+TEST(DYNAMIC_TEST, test2) {
     EXPECT_EQ(start_work("../tests/4.txt"), 1);
 }
 
-TEST(STATIC_TEST, test3) {
+TEST(DYNAMIC_TEST, test3) {
     EXPECT_EQ(start_work("../tests/5.txt"), -1);
 }
 
-TEST(STATIC_TEST, test4) {
+TEST(DYNAMIC_TEST, test4) {
     EXPECT_EQ(start_work("../tests/6.txt"), 1);
 }
 
-TEST(STATIC_TEST, test5) {
+TEST(DYNAMIC_TEST, test5) {
     EXPECT_EQ(start_work("../tests/7.txt"), 1);
 }
 
