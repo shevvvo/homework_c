2abac45f49d25e0e368d9bccb4ce9bddfb93f230
shevvvo <vasily.shevchenko2222222@gmail.com>
2021-11-08 22:32:57 +0300

    changed names x2

--- tests/lib_test.cpp
+++ tests/lib_test.cpp
@@ -8,7 +8,7 @@ extern "C" {
 TEST(LIB_TEST, wrong_filename) {
   long size = init_file_size("error");
   char *mass = init_massive_from_file(size, "error");
-  EXPECT_EQ(pre_working_initialize(mass, size), FILE_ERROR);
+  EXPECT_EQ(init_before_threading(mass, size), FILE_ERROR);
   free(mass);
 }
 
@@ -16,42 +16,42 @@ TEST(LIB_TEST, wrong_filename) {
 TEST(LIB_TEST, test) {
   long size = init_file_size("../data/1.txt");
   char *mass = init_massive_from_file(size, "../data/1.txt");
-  EXPECT_EQ(pre_working_initialize(mass, size), 1);
+  EXPECT_EQ(init_before_threading(mass, size), 1);
     free(mass);
 }
 
 TEST(LIB_TEST, test1) {
   long size = init_file_size("../data/2.txt");
   char *mass = init_massive_from_file(size, "../data/2.txt");
-  EXPECT_EQ(pre_working_initialize(mass, size), 1);
+  EXPECT_EQ(init_before_threading(mass, size), 1);
   free(mass);
 }
 
 TEST(LIB_TEST, test2) {
   long size = init_file_size("../data/3.txt");
   char *mass = init_massive_from_file(size, "../data/3.txt");
-  EXPECT_EQ(pre_working_initialize(mass, size), 1);
+  EXPECT_EQ(init_before_threading(mass, size), 1);
   free(mass);
 }
 
 TEST(LIB_TEST, test3) {
   long size = init_file_size("../data/4.txt");
   char *mass = init_massive_from_file(size, "../data/4.txt");
-  EXPECT_EQ(pre_working_initialize(mass, size), 1);
+  EXPECT_EQ(init_before_threading(mass, size), 1);
   free(mass);
 }
 
 TEST(LIB_TEST, test4) {
   long size = init_file_size("../data/5.txt");
   char *mass = init_massive_from_file(size, "../data/5.txt");
-  EXPECT_EQ(pre_working_initialize(mass, size), 1);
+  EXPECT_EQ(init_before_threading(mass, size), 1);
   free(mass);
 }
 
 TEST(LIB_TEST, test5) {
   long size = init_file_size("../data/6.txt");
   char *mass = init_massive_from_file(size, "../data/6.txt");
-  EXPECT_EQ(pre_working_initialize(mass, size), -1);
+  EXPECT_EQ(init_before_threading(mass, size), -1);
   free(mass);
 }
 
